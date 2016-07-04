a5b6e245f6153188fe22d32b90dcf4b6fa6d47dc
TonyProkop <prokop.tony@yahoo.com>
2016-07-04 14:02:19 -0500

    implemented servlet and model to handle bank request

--- Banking/src/model/Bank.java
+++ Banking/src/model/Bank.java
@@ -1,5 +1,60 @@
 package model;
 
-public class Bank {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+import util.DBUtil;
+
+public class Bank {
+	private int CardId;
+	private double Cost;
+	
+	public Bank(int cardId, double cost) {
+		super();
+		CardId = cardId;
+		Cost = cost;
+	}
+	
+	public int getCardId() {
+		return CardId;
+	}
+	public void setCardId(int cardId) {
+		CardId = cardId;
+	}
+	public double getCost() {
+		return Cost;
+	}
+	public void setCost(double cost) {
+		Cost = cost;
+	}
+
+	public static Boolean CheckBalance(Bank bankCheck) {
+		Boolean isValid = false; // flag for if the user has enough money for the transaction
+		Connection conn = DBUtil.getConnection();
+		try {
+			String query = "select * from CreditCards where CreditCards.Id = ?";
+			PreparedStatement prepState = conn.prepareStatement(query);
+			prepState.setInt(1, bankCheck.CardId);
+			ResultSet rs = prepState.executeQuery();
+			
+			rs.next();
+			double balance = rs.getDouble("Balance");
+			prepState.close();
+			
+			double newBalance = balance - bankCheck.getCost();
+			if(newBalance > 0) {
+				isValid = true;
+				query = "update CreditCards set Balance = ? where Id = ?";
+				prepState = conn.prepareStatement(query);
+				prepState.setDouble(1, newBalance);
+				prepState.setInt(2, bankCheck.getCardId());
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return isValid;
+	}
 }
--- Banking/src/servlet/Bank.java
+++ Banking/src/servlet/Bank.java
@@ -5,6 +5,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Bank
@@ -17,22 +18,27 @@ public class Bank extends HttpServlet {
      */
     public Bank() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		int cardId = Integer.parseInt(request.getParameter("cardId"));
+		double cost = Double.parseDouble(request.getParameter("cost"));
+		
+		model.Bank bankQuery = new model.Bank(cardId, cost);
+		Boolean successful = model.Bank.CheckBalance(bankQuery);
+		
+		// trying to respond with json
+		response.setContentType("application/json");
+		response.getWriter().write("transactionSuccess: " + successful.toString());
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
